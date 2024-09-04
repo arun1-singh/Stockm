326f088b3a64b4cfb1099bef5d7ed46f3ea9c494
arun1-singh <arunsingh17683@gmail.com>
2024-09-04 21:13:53 +0530

    Update route.js

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -4,12 +4,11 @@ import { MongoClient } from "mongodb";
 import {  NextResponse } from "next/server";
 export async  function GET(request) {
 
-    
+const query = request.nextUrl.searchParams.get("query")    
 const uri = "mongodb+srv://ABCDE:RgC1kwW1eh1Yac7q@cluster0.7mfe3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
  const client = new MongoClient(uri);
   try{
-   const searchParams = request.nextUrl.searchParams;
-   const query = searchParams.get("query");
+ 
      const database = client.db('stock');
      const inventory = database.collection('inventory');
 
